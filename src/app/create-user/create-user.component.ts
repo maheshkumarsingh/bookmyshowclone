9c285b121a92c00ad7d676dacbef197517f22855
v-mahessingh <v-mahessingh@microsoft.com>
2025-04-26 02:36:25 +0530

    enhanced create user

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { User } from '../_models/user';
@@ -7,7 +7,7 @@ import { UserService } from '../_services/user.service';
 @Component({
   selector: 'app-create-user',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor],
+  imports: [ReactiveFormsModule, NgIf, NgFor, CommonModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
@@ -15,9 +15,14 @@ export class CreateUserComponent implements OnInit {
 
   private fb = inject(FormBuilder);
   private userService = inject(UserService);
+
   userForm!: FormGroup;
   users: User[] = [];
 
+  // 🔁 State for interactivity
+  isSubmitting = false;
+  submissionMessage = '';
+  isSuccess = true;
 
   ngOnInit(): void {
     this.getAllUsers();
@@ -27,7 +32,8 @@ export class CreateUserComponent implements OnInit {
       address: ['', Validators.required]
     });
   }
-  getAllUsers(): void{
+
+  getAllUsers(): void {
     this.userService.getAllUsers().subscribe({
       next: (data: User[]) => {
         this.users = data;
@@ -37,22 +43,37 @@ export class CreateUserComponent implements OnInit {
       }
     });
   }
-  onCreateUserSubmit() {
-    if (this.userForm.valid) {
-      const newUser : User = this.userForm.value;
-      this.userService.createUser(newUser).subscribe({
-        next: (createUser) =>{
-          this.users.push(createUser);
-          this.userForm.reset;
-        },
-        error: (error) => console.error('User creation failed: ', error)
-      })
-    } else {
+
+  onCreateUserSubmit(): void {
+    if (this.userForm.invalid) {
       this.userForm.markAllAsTouched();
-      console.log('Form is invalid');
+      return;
     }
+
+    this.isSubmitting = true;
+    this.submissionMessage = '';
+
+    const newUser: User = this.userForm.value;
+
+    this.userService.createUser(newUser).subscribe({
+      next: (createdUser: User) => {
+        this.users.push(createdUser);
+        this.userForm.reset();
+        this.isSubmitting = false;
+        this.isSuccess = true;
+        this.submissionMessage = 'User created successfully!';
+      },
+      error: (error) => {
+        console.error('User creation failed:', error);
+        this.isSubmitting = false;
+        this.isSuccess = false;
+        this.submissionMessage = 'Failed to create user. Please try again.';
+      }
+    });
   }
-  onCancel() {
+
+  onCancel(): void {
     this.userForm.reset();
+    this.submissionMessage = '';
   }
 }
